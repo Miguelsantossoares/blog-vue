e835bb5acb8efcf42454155f9438e2458f83bef8
Miguel Santos <enium.miguel@gmail.com>
2024-07-23 08:44:30 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+</script>
+
+<template>
+  <nav>Navbar</nav>
+  <router-view />
+  <footer>footer</footer>
+</template>
+  
+<style scoped>
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <div>Home</div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Home",
+    setup () {
+        
+
+        return {}
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
