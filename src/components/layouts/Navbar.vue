4ff091765e7c6dd7b81c5f6a7337b0880e23fd28
Carlos da Silva <carlos@enium.com.br>
2024-07-27 12:57:57 -0300

    Designing Responsive Navbar

--- src/components/layouts/Navbar.vue
+++ src/components/layouts/Navbar.vue
@@ -1,45 +1,81 @@
 <template>
-    <nav id="navbar" class="px-4 py-4 sm:py-6 bg-white sm:bg-gray-900 sm:shadow-none sm:fixed w-full z-10">
-        <div class="container mx-auto flex flex-col sm:flex-row items-center justify-between">
-            <div class="w-full flex flex-row items-center justify-between">
-                <div class="text-3xl font-bold">
-                <span class="sm:text-white">Lara</span>
-                <span class="text-primary">Tips</span>
-            </div>
-
-            <div>
-                <button class="sm:hidden">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                     <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-                    </svg>
-                </button>
-
-                <button class="sm:hidden">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                     <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
-                    </svg>
-                </button>
-            </div>
-            </div>
-            <div class="mt-4 sm:mt-0 w-full sm:space-x-4 sm:text-right sm:block sm:text-white">
-                <NavbarLink to="/articles" label="Articles" />
-                <NavbarLink to="/about" label="About" />
-                <NavbarLink to="/contact" label="Contact" />
-            </div>
+  <nav
+    id="navbar"
+    class="px-4 py-4 sm:py-6 bg-white sm:bg-gray-900 sm:shadow-none sm:fixed w-full z-10"
+  >
+    <div class="container mx-auto flex flex-col sm:flex-row items-center justify-between">
+      <div class="w-full flex flex-row items-center justify-between">
+        <div class="text-3xl font-bold">
+          <span class="sm:text-white">Lara</span>
+          <span class="text-primary">Tips</span>
         </div>
-    </nav>
+
+        <div>
+          <button v-show="!isVisible" class="sm:hidden" @click="toggle">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="size-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+              />
+            </svg>
+          </button>
+
+          <button v-show="isVisible" class="sm:hidden" @click="toggle">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="size-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M6 18 18 6M6 6l12 12"
+              />
+            </svg>
+          </button>
+        </div>
+      </div>
+      <div
+        class="mt-4 sm:mt-0 w-full sm:space-x-4 sm:text-right sm:block sm:text-white"
+        :class="{ hidden: !isVisible }"
+      >
+        <NavbarLink to="/articles" label="Articles" />
+        <NavbarLink to="/about" label="About" />
+        <NavbarLink to="/contact" label="Contact" />
+      </div>
+    </div>
+  </nav>
 </template>
 
 <script>
 import NavbarLink from "./NavbarLink";
+import { useToggle } from "../../composables/useToggle";
 
 export default {
-components: {
-    NavbarLink
-},
+  components: {
+    NavbarLink,
+  },
 
-    props: {},
+  props: {},
 
-    setup() {},
+  setup() {
+    let { isVisible, toggle } = useToggle();
+
+    return {
+      isVisible,
+      toggle,
+    };
+  },
 };
 </script>
--- src/composables/userToggle.js
+++
@@ -1,5 +0,0 @@
-import { ref } from "vue";
-
-export function useToggle() {
-    let isVisible = ref(false)
-}
