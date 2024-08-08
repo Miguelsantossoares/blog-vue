118019f687914d8199fd174db667f55c01f3343f
Miguel Santos <enium.miguel@gmail.com>
2024-08-08 09:27:06 -0300

    Designing Contact Us Page complete

--- src/views/Contact/index.vue
+++ src/views/Contact/index.vue
@@ -37,9 +37,19 @@
                 </div>
 
                 <div>
-                    <textarea placeholder="Enter Message" class="px-4 py-2 w-full" rows="5"></textarea>
+                    <textarea placeholder="Enter Message" class="px-4 py-2 w-full border focus:border-primary focus:outline-none rounded bg-gray-200" rows="5" required></textarea>
                 <!-- <span class="text-red-600 text-sm">The error message</span> -->
                 </div>
+
+            <!--    <div class="px-4 py-2 bg-red-600 text-white rounded">
+                        Lorem ipsum dolor sit amet consectetur.
+                    </div> -->
+
+            <!--    <div class="px-4 py-2 bg-green-600 text-white rounded">
+                        Lorem ipsum dolor sit amet consectetur.
+                    </div> -->
+
+                <button class="px-4 py-2 sm:px-6 bg-primary text-white rounded focus:outline-none">Submit</button>
             </form>
             
         </div>
